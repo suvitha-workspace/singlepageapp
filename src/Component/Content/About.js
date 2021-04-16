653ab54dd1267da28fb4976a433f291bf393c5f0
suvitha-workspace <80394034+suvitha-workspace@users.noreply.github.com>
2021-04-16 23:41:42 +0530

    Create About.js

---
+++ src/Component/Content/About.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Section.css'
+class About extends Component {
+render() {
+return (
+<div className="condiv">
+<h1 className="subtopic">About Me</h1>
+<h4>Hey there,</h4>
+<h1>I'm Suvitha Ramasamy</h1>
+<h3>Full Stack Web <u>Developer</u></h3>
+<br></br>
+<p> I would like to
+creating Interactive UI components for better UX  and share those desgin and codes
+to the world through Github, Codepen and Instagram.
+</p>
+</div>
+)
+}
+}
+export default About
